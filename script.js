0d19031affb6d5d29957e8e52c8dc7c50c700774
David Sanchez <davidmsanchez250@gmail.com>
2021-01-15 20:32:14 -0500

    set more functionality to setQuestion function to include multiple choices. Began to write questions.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const startBtn = document.querySelector('#startBtn');
 const multipleChoiceQuestion = document.querySelector('#questionContainer');
-const potentialMultChoice = Array.from(document.querySelectorAll('.form-check-label'));
+const potentialMultChoice1 = document.querySelector('#choice1');
+const potentialMultChoice2 = document.querySelector('#choice2');
+const potentialMultChoice3 = document.querySelector('#choice3');
+const potentialMultChoice4 = document.querySelector('#choice4');
 const nextBtn = document.querySelector('#next')
 const timer = document.querySelector('#timer'); //setInterval()
 const scoreNumber = document.querySelector('#score');
 
 
-let currentQuestion = 0;
 let rightAnswer = true; //???
 let score = 0;
 let questionCounter = 0; //???
@@ -15,27 +17,27 @@ let questionsToChooseFrom = []; //???
 
 let potentialQuestions = [
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '7',
+        question: 'Which is used to store multiple values within one variable?',
+        choice1: 'Integer',
+        choice2: 'Array',
+        choice3: 'Let',
+        choice4: 'Const',
         answer: 2,
     },
     {
-        question: 'What is 2 + 3?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '7',
-        answer: 2,
+        question: 'What word is used to complete an if statement?',
+        choice1: 'then',
+        choice2: 'while',
+        choice3: 'else',
+        choice4: 'else if',
+        answer: 3,
     },
     {
-        question: 'What is 2 + 4?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '7',
+        question: 'What kind of operator is used to describe a value of true or false?',
+        choice1: 'Boolean',
+        choice2: 'String',
+        choice3: 'Number',
+        choice4: 'Object',
         answer: 2,
     },
     {
@@ -107,8 +109,18 @@ function startGame(){
 
 function setQuestion(){
 
+    let currentQuestion = 0;
+    let currentMultChoice = 0; 
+
+
     multipleChoiceQuestion.innerHTML = potentialQuestions[currentQuestion].question
     currentQuestion += 1
+
+    potentialMultChoice1.innerHTML = potentialQuestions[currentMultChoice].choice1
+    potentialMultChoice2.innerHTML = potentialQuestions[currentMultChoice].choice2
+    potentialMultChoice3.innerHTML = potentialQuestions[currentMultChoice].choice3
+    potentialMultChoice4.innerHTML = potentialQuestions[currentMultChoice].choice4
+    currentMultChoice += 1
 
 
     //let newQuestion = Math.floor(Math.random()*potentialQuestions.length)
