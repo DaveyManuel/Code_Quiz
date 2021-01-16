840f7fb31fc2d5687a679a4b677165897da69c40
David Sanchez <davidmsanchez250@gmail.com>
2021-01-15 21:21:48 -0500

    wrote questions for quiz

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ let rightAnswer = true; //???
 let score = 0;
 let questionCounter = 0; //???
 let questionsToChooseFrom = []; //???
+let currentQuestion = 0;
+let currentMultChoice = 0; 
 
 
 let potentialQuestions = [
@@ -22,7 +24,7 @@ let potentialQuestions = [
         choice2: 'Array',
         choice3: 'Let',
         choice4: 'Const',
-        answer: 2,
+       // answer: 2,
     },
     {
         question: 'What word is used to complete an if statement?',
@@ -30,71 +32,71 @@ let potentialQuestions = [
         choice2: 'while',
         choice3: 'else',
         choice4: 'else if',
-        answer: 3,
+        // answer: 3,
     },
     {
-        question: 'What kind of operator is used to describe a value of true or false?',
+        question: 'What kind of operator is used to describe if a value is true or false?',
         choice1: 'Boolean',
         choice2: 'String',
         choice3: 'Number',
         choice4: 'Object',
-        answer: 2,
+        // answer: 1,
     },
     {
-        question: 'What is 2 + 5?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '7',
-        answer: 2,
+        question: 'What JavaScript method do we use in order to turn a string into an integer?',
+        choice1: 'push',
+        choice2: 'concat',
+        choice3: 'parseInt',
+        choice4: 'toUpperCase',
+       // answer: 3,
     },
     {
-        question: 'What is 2 + 6?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '7',
-        answer: 2,
+        question: 'To successfully create a function, one must end the function with a ',
+        choice1: 'curly brace {}',
+        choice2: 'parenthesis ()',
+        choice3: 'bracket []',
+        choice4: 'period .',
+        // answer: 1,
     },
     {
-        question: 'What is 2 + 7?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '7',
-        answer: 2,
+        question: 'This function is primarily used for debugging purposes:',
+        choice1: 'concat',
+        choice2: 'printInfo',
+        choice3: 'find',
+        choice4: 'console.log',
+        // answer: 4,
     },
     {
-        question: 'What is 2 + 8?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '7',
-        answer: 2,
+        question: 'To refactor code is to...',
+        choice1: 'decrease repetition',
+        choice2: 'organize code',
+        choice3: 'simplify code',
+        choice4: 'all of the above',
+        // answer: 4,
     },
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '7',
-        answer: 2,
+        question: 'Which DOM method do we primarily use to select any element from our HTML',
+        choice1: 'concat',
+        choice2: 'querySelector',
+        choice3: 'find',
+        choice4: 'getElementbyId',
+        // answer: 2,
     },
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '7',
-        answer: 2,
+        question: 'The .setAttribute method is used to...',
+        choice1: 'add functionality to an element',
+        choice2: 'link our JS with our HTML',
+        choice3: 'add a class to an element',
+        choice4: 'none of the above',
+       // answer: 3,
     },
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '7',
-        answer: 2,
+        question: 'Which tag is used in HTML to link our JavaScript',
+        choice1: '<script></script>',
+        choice2: '<link>',
+        choice3: '<src>',
+        choice4: '<java></java>',
+        // answer: 1,
     },
 
 ]
@@ -109,18 +111,17 @@ function startGame(){
 
 function setQuestion(){
 
-    let currentQuestion = 0;
-    let currentMultChoice = 0; 
-
-
     multipleChoiceQuestion.innerHTML = potentialQuestions[currentQuestion].question
     currentQuestion += 1
+
 
     potentialMultChoice1.innerHTML = potentialQuestions[currentMultChoice].choice1
     potentialMultChoice2.innerHTML = potentialQuestions[currentMultChoice].choice2
     potentialMultChoice3.innerHTML = potentialQuestions[currentMultChoice].choice3
     potentialMultChoice4.innerHTML = potentialQuestions[currentMultChoice].choice4
     currentMultChoice += 1
+
+    
 
 
     //let newQuestion = Math.floor(Math.random()*potentialQuestions.length)
@@ -138,6 +139,7 @@ function nextQuestion(){
     //functionality for next button
 
 }
+
 
 nextQuestion();
 
